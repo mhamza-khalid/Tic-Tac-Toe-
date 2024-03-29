a6570954ed5b95db1db44f7bef292c6fc97d79e3
Muhammad Hamza Khalid <125656697+mhamza-khalid@users.noreply.github.com>
2024-03-30 01:33:46 +1000

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -1,59 +1,40 @@
 
-let board = (function(){ //board.getBoard()
+let board = (function () { //board.getBoard()
 
     let rows = 3;
     let columns = 3;
 
     let gameBoard = [];
 
-    for(let i=0; i<rows; i++){
+    for (let i = 0; i < rows; i++) {
         gameBoard[i] = [];
 
-        for(let j=0; j<columns; j++){
+        for (let j = 0; j < columns; j++) {
             gameBoard[i].push(addObjects());
         }
     }
 
-    function getBoard() {return gameBoard}
+    function getBoard() { return gameBoard }
 
-    return {getBoard}
+    return { getBoard }
 
 })();
 
-let players =(function(){ //players.getPlayers()[0].name = 'Player1'
 
-    let playersArray = [
-        {
-            name: 'Player 1',
-            marker: 'O'
-        },
-        {
-            name:'Player 2',
-            marker: 'X'
-        }
-    ]
+function addObjects() {
 
-    function getPlayers(){
-        return playersArray
-    }
-
-    return {getPlayers}
-})();
-
-function addObjects(){
-    
     //value = 0 is empty cell
     //value = 'O' is player1 marker
     //value = 'X' is player2 marker 
     let value = 0;
 
-    getValue = () => {return value}
+    getValue = () => { return value }
 
     updateValue = (player) => {
         value = player.marker;
     }
 
-    resetValue = () =>{
+    resetValue = () => {
         value = 0;
     }
 
@@ -64,209 +45,325 @@ function addObjects(){
     }
 }
 
+let players = (function () { //players.getPlayers()[0].name = 'Player1'
 
-function gameController(){
-
-    
-
-    let isGameOver = false;
-    let resetPlayer = false;  //after game over we have to set activeplayer to players[0]
-    let turns = 0;
-    let gameArray = [];
-
-    let disableClick = false;
-
-    // activePlayer is the player object playing current round
-    let activePlayer = players.getPlayers()[0]; 
-    let nextPlayer = players.getPlayers()[1];
-
-    switchPlayer = () =>{
-
-        if(resetPlayer){
-            activePlayer = players.getPlayers()[0];
-            resetPlayer = false;
+    let playersArray = [
+        {
+            name: 'Player 1',
+            marker: 'O',
+            winCount: 0,
+        },
+        {
+            name: 'Player 2',
+            marker: 'X',
+            winCount: 0,
         }
-        else{
-            activePlayer = activePlayer == players.getPlayers()[0] ? players.getPlayers()[1] : players.getPlayers()[0];
+    ]
 
-            nextPlayer = nextPlayer == players.getPlayers()[1] ? players.getPlayers()[0] : players.getPlayers()[1];
-        }
+
+    return playersArray
+})();
+
+
+function gameController() {
+
+    //closure variables
+    let activePlayer = players[0]; 
+    let winnerFlag = false;
+    let noOfTurns = 0;
+    let roundNumber = 1;
+
+    getTurnsNo = () => {return noOfTurns}
+
+    setTurnsNo = () => {noOfTurns = 0}
+
+    getWinnerFlag = () => {return winnerFlag}
+
+    setWinnerFlag = () => {winnerFlag = false}
+
+    getActivePlayer = () => { return activePlayer };
+
+    switchPlayer = () => {
+
+        activePlayer = activePlayer == players[0] ? players[1] : players[0];
 
     }
 
-    displayBoardValues = () =>{
+    displayPlayer = () => {
+        console.log('Its ' + activePlayer.name + ' turn' + ' ' + 'with marker ' + activePlayer.marker)
+        
+    }
 
-        let boardValues = board.getBoard().map((outer)=> outer.map((inner)=>inner.getValue()));
+    displayBoardValues = () => {
+
+        let boardValues = board.getBoard().map(outer => outer.map(inner => inner.getValue()))
         console.log(boardValues);
     }
 
-    displayPlayerTurn = () =>{
-
-        if(disableClick == false){
-        let markerDisplay = document.querySelector('.turn--display')
-        markerDisplay.textContent = nextPlayer.marker + 's Turn';
-        }
-        else{ return }
-
+    clearBoard = () =>{
+            
+     for (let i=0; i<3; i++){
+         for (let j=0; j<3; j++){
+             board.getBoard()[i][j].resetValue();
+         }
+     }
     }
 
-    checkWinner = () =>{
+    checkTie = () =>{
+        if(noOfTurns == 9 && winnerFlag == false){
+            console.log('Game Tied!');
+            clearBoard();
+            //noOfTurns = 0;
+            //++roundNumber;
+        }
+    }
 
-        let comb = [0,1,2];
-
+    checkWinner = () => {
+        let comb = [0, 1, 2];
+        // console.log(comb);
         // first 2 for loops check if 3 in a row in any column or row
         // 3rd loop and 4th checks diagonaly
         // for reference the co-ordinates of board.getBoard() are
         // [ [(0,0), (0,1), (0,2)],
         // [(1,0), (1,1), (1,2)],
         // [(2,0), (2,1), (2,2)] ]
-
-        for (let i=0; i<3; i++){
-            if(board.getBoard()[i][comb[0]].getValue() == board.getBoard()[i][comb[1]].getValue() && board.getBoard()[i][comb[1]].getValue() == board.getBoard()[i][comb[2]].getValue() && board.getBoard()[i][comb[0]].getValue() != 0 ){
+        for (let i = 0; i < 3; i++) {
+            if (board.getBoard()[i][comb[0]].getValue() == board.getBoard()[i][comb[1]].getValue() && board.getBoard()[i][comb[1]].getValue() == board.getBoard()[i][comb[2]].getValue() && board.getBoard()[i][comb[0]].getValue() != 0) {
                 console.log('Winner is' + ' ' + activePlayer.name);
-                isGameOver = true;
-                gameArray.push[1]
+                activePlayer.winCount += 1;
+                clearBoard();
+                winnerFlag = true;
+                noOfTurns = 0;  // each round has max 9 turns
+                ++roundNumber;  //move to next round
+                alert(activePlayer.name + ' Wins this round')
             }
         }
-    
-        for (let j=0; j<3; j++){
-            if(board.getBoard()[comb[0]][j].getValue() == board.getBoard()[comb[1]][j].getValue() && board.getBoard()[comb[1]][j].getValue() == board.getBoard()[comb[2]][j].getValue() && board.getBoard()[comb[0]][j].getValue() != 0 ){
+        for (let j = 0; j < 3; j++) {
+            if (board.getBoard()[comb[0]][j].getValue() == board.getBoard()[comb[1]][j].getValue() && board.getBoard()[comb[1]][j].getValue() == board.getBoard()[comb[2]][j].getValue() && board.getBoard()[comb[0]][j].getValue() != 0) {
                 console.log('Winner is' + ' ' + activePlayer.name);
-                isGameOver = true;
-                gameArray.push[1]
+                activePlayer.winCount += 1;
+                clearBoard();
+                winnerFlag = true;
+                noOfTurns = 0;
+                ++roundNumber;
+                alert(activePlayer.name + ' Wins this round')
             }
         }
-        for(let k=0 ; k<1; k++){
-            if(board.getBoard()[k][k].getValue() == board.getBoard()[k+1][k+1].getValue() && board.getBoard()[k+1][k+1].getValue() == board.getBoard()[k+2][k+2].getValue() && board.getBoard()[k][k].getValue() != 0){
+        for (let k = 0; k < 1; k++) {
+            if (board.getBoard()[k][k].getValue() == board.getBoard()[k + 1][k + 1].getValue() && board.getBoard()[k + 1][k + 1].getValue() == board.getBoard()[k + 2][k + 2].getValue() && board.getBoard()[k][k].getValue() != 0) {
                 console.log('Winner is' + ' ' + activePlayer.name);
-                isGameOver = true;
-                gameArray.push[1]
+                activePlayer.winCount += 1;
+                clearBoard();
+                winnerFlag = true;
+                noOfTurns = 0;
+                ++roundNumber;
+                alert(activePlayer.name + ' Wins this round')
             }
         }
-        for(let m=0; m<1; m++){
-            if(board.getBoard()[m][m+2].getValue() == board.getBoard()[m+1][m+1].getValue() && board.getBoard()[m+1][m+1].getValue() == board.getBoard()[m+2][m].getValue() && board.getBoard()[m][m+2].getValue() != 0 ){
+        for (let m = 0; m < 1; m++) {
+            if (board.getBoard()[m][m + 2].getValue() == board.getBoard()[m + 1][m + 1].getValue() && board.getBoard()[m + 1][m + 1].getValue() == board.getBoard()[m + 2][m].getValue() && board.getBoard()[m][m + 2].getValue() != 0) {
                 console.log('Winner is' + ' ' + activePlayer.name);
-                isGameOver = true;
-                gameArray.push[1]
+                activePlayer.winCount += 1;
+                clearBoard();
+                winnerFlag = true;
+                noOfTurns = 0;
+                ++roundNumber;
+                alert(activePlayer.name + ' Wins this round')
             }
-         }
-    }
-    
-    checkTie = () =>{
-        ++turns   // so on every playGame() run, it means user has taken a turn
-                  // if 9 turns are taken and there is no winner, it means we have a tie
-        if(turns == 9){
-            console.log('Game Tied!')
-            gameArray.push[true]
         }
+
     }
 
-    gameOver = () =>{
-
-        //once game is over we change each object in board.getBoard() array, we 
-        //change its value to 0. 0 means that that position in array is empty (no marker in it)
-            for(let i=0; i<3; i++){
-        
-                for(let j=0; j<3; j++){
-                    board.getBoard()[i][j].resetValue();
-                }
-            }
-            resetPlayer = true;
-            isGameOver = false;  // reset flags and turns
-            turns = 0;
-        
+    showRound = () =>{
+        console.log('Round Number: ' + roundNumber);
     }
 
-    updateScreen =() => {
-   
-        // let play = gameController();
-    
-        let buttons = document.querySelectorAll('.buttons');
-    
-        buttons.forEach((button)=>{
-    
-        
-            button.addEventListener('click', (event)=>{
-
-                ++turns
-    
-                let row = event.target.getAttribute('row');
-                let column = event.target.getAttribute('column');
-    
-                
-                playRound(row,column);
-    
-                let marker = board.getBoard()[row][column].getValue();
-                
-                if(disableClick == false){
-                event.target.textContent = marker;
-                }
-
-                if(isGameOver == true){
-                    gameOver();
-                    let markerDisplay = document.querySelector('.turn--display')
-                    markerDisplay.textContent = 'Winner is ' + nextPlayer.marker;
-                    disableClick = true;
-                    checkTie();
-                }
-                if(turns == 9){
-                    let markerDisplay = document.querySelector('.turn--display')
-                    markerDisplay.textContent = 'Game Tied';
-                }
-            })
-        })
+    showPoints = () =>{
+        console.log(players[0].name + ' score:' + players[0].winCount)
+        console.log(players[1].name + ' score:' + players[1].winCount)
     }
 
-    function playRound(row, column) {
+    function playGame(row, column, currentPlayer) {
+        //ensures cell isn't occupied
+        if (board.getBoard()[row][column].getValue() == 0) {
 
-        //go to the gameBoard
-        //find the object in that row and column
-        //that object has three functions to update value (each object has its own value either 0, 'O' or 'X')
-        //update the value by calling updateValue which sets value to activePlayers marker
-        //now that objects value has been updated in the board 2D array
+            board.getBoard()[row][column].updateValue(getActivePlayer());
+            ++noOfTurns;
 
-        //checks if cell is empty or not
-        if(board.getBoard()[row][column].getValue() != 0){
-            return
+            //displayPlayer();
+            showRound();
+            showPoints();
+
+            displayBoardValues();
+            checkWinner();
+            checkTie();
+            switchPlayer();
+
         }
-        board.getBoard()[row][column].updateValue(activePlayer);
-
-        //display to console whose turn it is and current marker
-        displayPlayerTurn();
-
-        //check win conditions
-        checkWinner();
-
-        //display to console an array which contains the values of the board array
-        displayBoardValues();
-        
-        
-
-        //check if game is tie
-        //checkTie();
-
-        //check if game is over
-        //gameOver();
-
-        //now for next round, we have to switch player
-        switchPlayer();
-
+        else { return }
     }
 
-    return { updateScreen }
+    return { playGame, displayPlayer, getActivePlayer, getWinnerFlag, setWinnerFlag, checkTie,getTurnsNo, setTurnsNo, clearBoard}
 }
+
+formLoad();
+
+function formLoad(){
+
+    let modal = document.querySelector('#modal');
+    let form = document.querySelector('.form');
+
+    let player1NameDisplay = document.querySelector('#name1');
+    let player1MarkerDisplay = document.querySelector('#marker1');
+    let player1ScoreDisplay = document.querySelector('#score1');
+
+    
+
+    let player2NameDisplay = document.querySelector('#name2');
+    let player2MarkerDisplay = document.querySelector('#marker2');
+    let player2ScoreDisplay = document.querySelector('#score2');
+
+    
+
+    modal.showModal();
+
+    form.addEventListener('submit', (event)=>{
+        
+        let playerOneName = document.querySelector('#player1Name').value;
+        let playerTwoName = document.querySelector('#player2Name').value;
+
+        let display = document.querySelector('.turn--display');
+
+        let playerOneMarker = document.querySelector('#markerOne').value;
+        let playerTwoMarker = document.querySelector('#markerTwo').value;
+
+        
+
+        if(playerOneMarker == playerTwoMarker){
+            alert('Both players cant have the same markers!')
+            event.preventDefault();
+        }else{
+        players[0].name = playerOneName;
+        players[1].name = playerTwoName;
+
+        players[0].marker = playerOneMarker;
+        players[1].marker = playerTwoMarker;
+
+        player1NameDisplay.textContent = playerOneName;
+        player2NameDisplay.textContent = playerTwoName;
+
+        player1MarkerDisplay.textContent = "Marker:" + "'"  + playerOneMarker + "'";
+        player2MarkerDisplay.textContent = "Marker:" + "'" + playerTwoMarker + "'";
+
+        player1ScoreDisplay.textContent = 'Score: 0'
+        player2ScoreDisplay.textContent = 'Score: 0'
+
+        display.textContent = 'Its ' + players[0].name + "'s" + ' turn';
+        }
+    })
+}
+
+modal.addEventListener('keydown', function(event) {
+    
+    if (event.keyCode === 27) {
+        
+        event.preventDefault();
+    }
+});
+
 
 let play = gameController();
 
-play.updateScreen();
+let cells = document.querySelectorAll('.buttons')
+
+cells.forEach((cell)=>{
+
+    cell.addEventListener('click', (event)=>{
+
+        let row = event.target.getAttribute('row');
+        let column = event.target.getAttribute('column');
+        let playerMarker = play.getActivePlayer().marker;
+        
+        let display = document.querySelector('.turn--display');
+        
+
+        play.displayPlayer();
+        play.playGame(row, column);
+
+        let playerName = play.getActivePlayer().name;
+        event.target.textContent = playerMarker;
+
+        display.textContent = 'Its ' + playerName + "'s" + ' turn';
+
+        if(play.getWinnerFlag()){
+            cells.forEach((cell) =>{
+                cell.textContent = '\u00a0';
+            })
+            play.setWinnerFlag();
+            let player1ScoreDisplay = document.querySelector('#score1');
+            let player2ScoreDisplay = document.querySelector('#score2');
+
+            player1ScoreDisplay.textContent = 'Score: '+ players[0].winCount;
+            player2ScoreDisplay.textContent = 'Score: '+ players[1].winCount;
+        }
+        if(play.getTurnsNo() == 9 && play.getWinnerFlag() == false){
+            alert('Game Tied!');
+            cells.forEach((cell) =>{
+                cell.textContent = '\u00a0';
+            })
+            play.setTurnsNo();
+        }
+    })
+})
 
 let reset = document.querySelector('.reset')
 
 reset.addEventListener('click', ()=>{
+    let cells = document.querySelectorAll('.buttons');
+    cells.forEach((cell) =>{
+        cell.textContent = '\u00a0';
+    })
+    play.clearBoard();
+    players[0].winCount = 0;
+    players[1].winCount = 0;
+    let player1ScoreDisplay = document.querySelector('#score1');
+    let player2ScoreDisplay = document.querySelector('#score2');
 
-    location.reload(true);
-    
+    player1ScoreDisplay.textContent = 'Score: '+ players[0].winCount;
+    player2ScoreDisplay.textContent = 'Score: '+ players[1].winCount;
 })
+
+let restart = document.querySelector('.restart')
+
+restart.addEventListener('click', ()=>{
+    location.reload();
+})
+
+
+
+
+
+
+
+
+// play.playGame(0, 2);  // Tie Game moves
+// play.playGame(1, 2);
+// play.playGame(2, 2);
+// play.playGame(2, 1);
+// play.playGame(2, 0);
+// play.playGame(1, 1);
+// play.playGame(1, 0);
+// play.playGame(0, 0);
+// play.playGame(0, 1);
+
+// play.playGame(0,0);    // O wins moves
+// play.playGame(0,1);
+// play.playGame(1,0);
+// play.playGame(1,1);
+// play.playGame(2,0);
+
+
+
 
 
 
